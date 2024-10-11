e9edb37b94f9988e34fb66a8efb0ea3fbe6a1f77
gws <glennws@pm.me>
2024-10-11 14:42:52 +0200

    setup rust toolchain

--- flake.nix
+++ flake.nix
@@ -3,13 +3,36 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  outputs = {
+    self,
+    nixpkgs,
+    fenix,
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [fenix.overlays.default];
+    };
+    toolchain = pkgs.fenix.combine [
+      pkgs.cargo
+      pkgs.rustc
+      pkgs.rust-analyzer
+    ];
+  in {
+    packages.${system} = {
+      hello = pkgs.hello;
+      default = self.packages.x86_64-linux.hello;
+    };
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [
+        toolchain
+      ];
+    };
   };
 }
