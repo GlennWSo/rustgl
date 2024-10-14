fb35d323343f192ec5b13a9457d349d961f511c4
gws <glennws@pm.me>
2024-10-14 13:39:29 +0200

    blue screen

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,15 @@
 use glium::{
-    uniform,
+    implement_vertex, uniform,
     winit::event::{Event, WindowEvent},
     DrawParameters, Frame, Surface,
 };
 
-mod teapot;
-
-/// Turning the coordinates relative to the scene into
-/// coordinates relative to the camera's position and rotation.
-fn view_matrix(position: &[f32; 3], direction: &[f32; 3], up: &[f32; 3]) -> [[f32; 4]; 4] {
-    let f = {
-        let f = direction;
-        let len = f[0] * f[0] + f[1] * f[1] + f[2] * f[2];
-        let len = len.sqrt();
-        [f[0] / len, f[1] / len, f[2] / len]
-    };
-
-    let s = [
-        up[1] * f[2] - up[2] * f[1],
-        up[2] * f[0] - up[0] * f[2],
-        up[0] * f[1] - up[1] * f[0],
-    ];
-
-    let s_norm = {
-        let len = s[0] * s[0] + s[1] * s[1] + s[2] * s[2];
-        let len = len.sqrt();
-        [s[0] / len, s[1] / len, s[2] / len]
-    };
-
-    let u = [
-        f[1] * s_norm[2] - f[2] * s_norm[1],
-        f[2] * s_norm[0] - f[0] * s_norm[2],
-        f[0] * s_norm[1] - f[1] * s_norm[0],
-    ];
-
-    let p = [
-        -position[0] * s_norm[0] - position[1] * s_norm[1] - position[2] * s_norm[2],
-        -position[0] * u[0] - position[1] * u[1] - position[2] * u[2],
-        -position[0] * f[0] - position[1] * f[1] - position[2] * f[2],
-    ];
-
-    [
-        [s_norm[0], u[0], f[0], 0.0],
-        [s_norm[1], u[1], f[1], 0.0],
-        [s_norm[2], u[2], f[2], 0.0],
-        [p[0], p[1], p[2], 1.0],
-    ]
+#[derive(Copy, Clone)]
+pub struct Vertex {
+    position: [f32; 2],
 }
-fn perspective(frame: &Frame) -> [[f32; 4]; 4] {
-    let (width, height) = frame.get_dimensions();
-    let aspect_ratio = height as f32 / width as f32;
 
-    let fov: f32 = 3.141592 / 3.0;
-    let zfar = 1024.0;
-    let znear = 0.1;
-
-    let f = 1.0 / (fov / 2.0).tan();
-
-    [
-        [f * aspect_ratio, 0.0, 0.0, 0.0],
-        [0.0, f, 0.0, 0.0],
-        [0.0, 0.0, (zfar + znear) / (zfar - znear), 1.0],
-        [0.0, 0.0, -(2.0 * zfar * znear) / (zfar - znear), 0.0],
-    ]
-}
+implement_vertex!(Vertex, position);
 
 fn main() {
     let event_loop = glium::winit::event_loop::EventLoop::builder()
@@ -71,14 +17,17 @@ fn main() {
         .expect("event loop building");
     let (window, display) = glium::backend::glutin::SimpleWindowBuilder::new().build(&event_loop);
 
-    let positions = glium::VertexBuffer::new(&display, &teapot::VERTICES).unwrap();
-    let normals = glium::VertexBuffer::new(&display, &teapot::NORMALS).unwrap();
-    let indices = glium::IndexBuffer::new(
-        &display,
-        glium::index::PrimitiveType::TrianglesList,
-        &teapot::INDICES,
-    )
-    .unwrap();
+    let tri = [
+        Vertex {
+            position: [-1.0, -1.0],
+        },
+        Vertex {
+            position: [3.0, -1.0],
+        },
+        Vertex {
+            position: [-1.0, 3.0],
+        },
+    ];
 
     let program =
         glium::Program::from_source(&display, VERTEX_SHADER, FRAGMENT_SHADER, None).unwrap();
@@ -104,27 +53,24 @@ fn main() {
                         [0.0, 0.0, s, 0.0],
                         [0.0, 0.0, 2.0, 1.0f32],
                     ];
-                    let view = view_matrix(&[2.0, -1.0, 1.0], &[-2.0, 1.0, 1.0], &[0.0, 1.0, 0.0]);
-                    let uniforms = uniform! {
-                        u_light: light,
-                        model: model_transform,
-                        perspective: perspective(&frame),
-                        view: view,
-                    };
+                    let uniforms = uniform! {};
 
                     let draw_parameters = DrawParameters {
-                        depth: glium::Depth {
-                            test: glium::DepthTest::IfLess,
-                            write: true,
-                            ..Default::default()
-                        },
+                        // depth: glium::Depth {
+                        //     test: glium::DepthTest::IfLess,
+                        //     write: true,
+                        //     ..Default::default()
+                        // },
                         ..Default::default()
                     };
 
+                    let vertex_buff = glium::VertexBuffer::new(&display, &tri).unwrap();
+                    let no_indices =
+                        glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
                     frame
                         .draw(
-                            (&positions, &normals),
-                            &indices,
+                            &vertex_buff,
+                            &no_indices,
                             &program,
                             &uniforms,
                             &draw_parameters,
@@ -143,42 +89,18 @@ fn main() {
 
 const VERTEX_SHADER: &'static str = r#"
     #version 150
-    in vec3 position;
-    in vec3 normal;
-    out vec3 v_normal;
-    out vec3 v_position;
+    in vec2 position;
 
-    uniform mat4 model;
-    uniform mat4 view;
-    uniform mat4 perspective;
     
     void main() {
-        // vertex_color = color;
-        v_normal = transpose(inverse(mat3(model))) * normal;
-        gl_Position = perspective*view*model * vec4(position, 1.0);
-        v_position = gl_Position.xyz / gl_Position.w;
+        gl_Position = vec4(position, 0.0, 1.0);
     }
 "#;
 
 const FRAGMENT_SHADER: &'static str = r#"
     #version 140
-    in vec3 v_normal;
-    in vec3 v_position;
-
-    out vec4 color;
-    uniform vec3 u_light;
-
-
-    const vec3 ambient_color = vec3(0.2, 0.0, 0.0);
-    const vec3 diffuse_color = vec3(0.6, 0.0, 0.0);
-    const vec3 specular_color = vec3(1.0, 1.0, 1.0);
     
     void main() {
-        float diffuse = max(dot(normalize(u_light), normalize(v_normal)), 0.0);
-
-        vec3 camera_dir = normalize(-v_position);
-        vec3 half_direction = normalize(normalize(u_light) + camera_dir);
-        float specular = pow(max(dot(half_direction, normalize(v_normal)), 0.0), 16.0);
-
-        color = vec4(ambient_color + diffuse * diffuse_color + specular * specular_color, 1.0);    }
+        gl_FragColor = vec4(0.0, 0.0, 1.0, 1.0);
+    }
 "#;
